188692b5b98a8d61a89836c8de84e103ee4242ae
violllin <izhboldina_v@mail.ru>
2025-01-12 15:15:25 +0700

    Созданы базовые классы.

---
+++ SupplierRequestsApp/Domain/Models/Supplier.cs
@@ -0,0 +1,42 @@
+﻿namespace SupplierRequestsApp.Domain.Models;
+
+public class Supplier
+{
+    private Guid _id;
+    private string _name;
+    private string _address;
+    private string _phone;
+
+
+    public Supplier(Guid id, string name, string address, string phone)
+    {
+        _id = id;
+        _name = name;
+        _address = address;
+        _phone = phone;
+    }
+
+    public Guid Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public string Address
+    {
+        get => _address;
+        set => _address = value;
+    }
+
+    public string Phone
+    {
+        get => _phone;
+        set => _phone = value;
+    }
+}
