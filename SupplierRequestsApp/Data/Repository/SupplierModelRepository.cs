b64392d907f161a20c3263c57159d7577756e397
violllin <izhboldina_v@mail.ru>
2025-01-29 18:02:34 +0700

    Created IModelRepository.cs  - Made SupplierModelRepository.cs

---
+++ SupplierRequestsApp/Data/Repository/SupplierModelRepository.cs
@@ -0,0 +1,37 @@
+﻿using SupplierRequestsApp.Data.Service;
+using SupplierRequestsApp.Domain.Models;
+using SupplierRequestsApp.Domain.Repository;
+using SupplierRequestsApp.Domain.Service;
+
+namespace SupplierRequestsApp.Data.Repository;
+
+public class SupplierModelRepository : IModelRepository<Supplier>
+{
+    private readonly IStorage<Supplier> _storageService = new LocalStorageService<Supplier>();
+    private static readonly Type ModelType = typeof(Supplier);
+
+    public Supplier? LoadEntity(Guid entityId)
+    {
+        return _storageService.LoadEntity(ModelType, entityId.ToString());
+    }
+
+    public IEnumerable<Supplier> LoadEntities()
+    {
+        return _storageService.LoadEntities(ModelType);
+    }
+
+    public void AddEntity(Supplier entity)
+    {
+        _storageService.SaveEntity(entity);
+    }
+
+    public void RemoveEntity(Supplier entity)
+    {
+        _storageService.DropEntity(entity);
+    }
+
+    public void EditEntity(Supplier updatedEntity)
+    {
+        _storageService.UpdateEntity(updatedEntity);
+    }
+}
---
+++ SupplierRequestsApp/Domain/Repository/IModelRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SupplierRequestsApp.Domain.Repository;
+
+public interface IModelRepository<T> where T: class
+{
+    public T? LoadEntity(Guid entityId);
+    public IEnumerable<T> LoadEntities();
+    public void AddEntity(T entity);
+    public void RemoveEntity(T entity);
+    public void EditEntity(T updatedEntity);
+}
